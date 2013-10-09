ecd43de6a99ff3b9cadee7bff26bccfacdad9c08
Hayden William Arie Van Reyswoud <h.v.reyswoud@gmail.com>
2013-10-09 22:39:00 +1100

    added team testHunterView file

---
+++ testHunterView3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+#include "cities.h"
+#include "game.h"
+#include "HunterView.h"
+
+#define MAX_LOCATION 71
+
+void haydenTestFunc();
+void julianTestFunc();
+
+//TeamSegItsYourFault's test program HunterView.h
+int main() {
+	printf("Testing with Hayden's tests\n");
+    haydenTestFunc();
+	printf("Passed Hayden's tests!\nNow testing with Julian's tests\n");
+	julianTestFunc();
+	printf("Passed Hayden's tests!\nCongrats, all tests passed!\n");
+	return 0;
+}
+
+void haydenTestFunc() {
+
+}
+
+void julianTestFunc() {
+
+}
