d8e2ad91968b9d08219e89e44d963b1e10a31a41
Hayden William Arie Van Reyswoud <h.v.reyswoud@gmail.com>
2013-10-09 13:11:44 +1100

    new files uploaded

--- HunterView.h
+++ HunterView.h
@@ -74,26 +74,62 @@ void disposeHunterView (HunterView toBeDeleted);
 Round getRound (HunterView currentView);
 
 //Get the id of current player - ie whose turn is it?
+// Only returns a 'playerID' which is one of:
+//   LORD_GODALMING (0): Lord Godalming's turn
+//   DR_SEWARD      (1): Dr. Seward's turn
+//   VAN_HELSING    (2): Van Helsing's turn
+//   MINA_HARKER    (3): Mina Harker's turn
+//   DRACULA        (4): Dracula's turn
 PlayerID getCurrentPlayer (HunterView currentView);
 
 //Get the current score
+// Returns a positive integer [0...366]
 int getScore(HunterView currentView);
 
 //Get the current health points for a given player
-int getHealth(HunterView currentView, PlayerID);
+// 'player' specifies which players's life/blood points to return
+//    and must be a value in the interval [0...4] (see 'player' type)
 
-//Get the current location id of a given player
-//May be NO_LOCATION if the player has not had a turn yet
-//(ie at the beginning of the game
-//Or for dracula it may be UNKNOWN_CITY or UNKNOWN_SEA
-LocationID getLocation(HunterView currentView, PlayerID);     
+int getHealth(HunterView currentView, PlayerID player);
+
+// Get the current location id of a given player
+// May be UNKNOWN_LOCATION if the player has not had a turn yet
+// (ie at the beginning of the game when the round is 0)
+// otherwise for a hunter it should be an integer in the interval [0..70] 
+// The given roundNumber is >= 0 and <= the current round number
+// 'whichHunter' specifies which hunter's location to return
+//    and must be a value in the interval [0...3] (see 'player' type)
+// Or for dracula it should 
+// gets the location of Dracula at the start of a particular round
+// Returns an integer:
+//   in the interval [0...70] if Dracula was known to be in a city or sea
+//   CITY_UNKNOWN     if Dracula was in an unknown city
+//   SEA_UNKNOWN      if Dracula was in an unknown sea
+//   HIDE             if Dracula was known to have made a hide move
+//   DOUBLE_BACK_N    where N is [0...5], if Dracula was known to have 
+//                    made a double back move N positions back in the trail
+//                    e.g. DOUBLE_BACK_1 is the last place place he visited
+//   TELEPORT         if Dracula apparated back to Castle Dracula
+//   LOCATION_UNKNOWN if the round number is 0
+LocationID getLocation(HunterView currentView, PlayerID player);     
 
 //Functions that return information about the history of the game
 
 // Fills the trail array with the location ids of the last 6 turns for the given player
-// For dracula this may include other locations CITY_UNKNOWN or SEA_UNKNOWN
+// For dracula this may include integers:
+//   in the interval [0...70] if Dracula was known to be in a city or sea
+//   CITY_UNKNOWN     if Dracula was in an unknown city
+//   SEA_UNKNOWN      if Dracula was in an unknown sea
+
+//   HIDE             if Dracula was known to have made a hide move
+//   DOUBLE_BACK_N    where N is [0...5], if Dracula was known to have
+//                    made a double back move N positions back in the trail
+//                    e.g. DOUBLE_BACK_1 is the last place place he visited
+//   TELEPORT         if Dracula apparated back to Castle Dracula
+
+
 // For any player if the move does not exist yet (i.e, the start of the game),
-// the value should be NO_LOCATION (-1)
+// the value should be UNKNOWN_LOCATION (-1)
 // For example after 2 turns the array may have the contents
 // {29, 182, -1, -1, -1, -1} 
 // This would mean in the first move the player started on location 182 
@@ -111,7 +147,7 @@ void getHistory (HunterView currentView, PlayerID player,LocationID trail[TRAIL_
 //Your function must take into account the round and player id for rail travel
 //Your function must take into account that dracula can't move to the hospital or travel by rail
 //but need not take into account draculas trail
-//Any location that the player is currently in, should be included.
+//The destination 'from' should be included.
 LocationID * connectedLocations(HunterView currentView, int * numLocations, LocationID from, 
                               PlayerID player, Round round, int road, int rail, int sea);
 
