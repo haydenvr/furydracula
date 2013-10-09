426b0f919c258ad43b40ebdf05913e2e0e028b25
Julian Blair <jubian@me.com>
2013-10-09 14:58:17 +1100

    fixed messages

--- HunterView.c
+++ HunterView.c
@@ -4,49 +4,6 @@
 #include "HunterView.h"
 #include "graph/Graph.h"
 
-#ifndef HUNTER_VIEW_H
-#define HUNTER_VIEW_H
-
-#include "cities.h"
-
-#define FALSE 0
-#define TRUE 1
-
-// Players
-#define PLAYER_LORD_GODALMING   0
-#define PLAYER_DR_SEWARD        1
-#define PLAYER_VAN_HELSING      2
-#define PLAYER_MINA_HARKER      3
-#define PLAYER_DRACULA          4
-
-#define NUM_PLAYERS     5
-
-#define TRAIL_SIZE 6
-
-//Score
-#define GAME_START_SCORE                366
-#define SCORE_LOSS_DRACULA_TURN         1
-#define SCORE_LOSS_HUNTER_HOSPITAL      6
-#define SCORE_LOSS_VAMPIRE_MATURES      13
-
-// Dracula Health
-#define GAME_START_BLOOD_POINTS         40
-#define LIFE_LOSS_HUNTER_ENCOUNTER      10
-#define LIFE_LOSS_SEA                   2
-#define LIFE_GAIN_CASTLE_DRACULA        10
-
-//Hunter Health
-#define GAME_START_HUNTER_LIFE_POINTS   9
-#define LIFE_LOSS_TRAP_ENCOUNTER        2
-#define LIFE_LOSS_DRACULA_ENCOUNTER     4
-#define LIFE_GAIN_REST                  3 
-
-     
-typedef struct hunterView *HunterView;
-     
-typedef int PlayerID;
-typedef int Round;
-
 typedef struct player *Player;
      
 struct hunterView {
