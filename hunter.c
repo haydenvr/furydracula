393532b36f5a0eaef80d50bda17ba54b64c80b2a
U-DJSF\Julian <Julian@DJSF.(none)>
2013-10-21 22:50:57 +1100

    small fix

--- hunter.c
+++ hunter.c
@@ -39,54 +39,52 @@ void decideMove (HunterView gameState) {
     int camper = 0, i;
     
     //check for campers OTHER THAN CURRENT
-    for (i = 0; i < NUM_HUNTERS; i++) if (i != id && getLocation(gameState, i) == CASTLE_DRACULA) camper = 1;   
-    
-    //continue camping if no other current campers
-    if (move == CASTLE_DRACULA) {
-        if (camper) move = KLAUSENBURG;
+    for (i = 0; i < NUM_HUNTERS; i++) if (i != id && getLocation(gameState, i) == CASTLE_DRACULA) camper = 1;     
+    if (!camper) {
+        //if no camper and hunter is shortest dist to castle dracula, move towards castle dracula
+        int hunterDist[NUM_HUNTERS] = {UNKNOWN_LOCATION,UNKNOWN_LOCATION,UNKNOWN_LOCATION,UNKNOWN_LOCATION};
+        int closestHunter = PLAYER_LORD_GODALMING;
+        LocationID adj;
+        for (i = PLAYER_LORD_GODALMING; i < NUM_HUNTERS; i++) {
+            hunterDist[i] = findShortestPath(getLocation(gameState, i), CASTLE_DRACULA, path, ANY, round);
+            if ((hunterDist[closestHunter] > hunterDist[i]) || (hunterDist[closestHunter] == UNKNOWN_LOCATION)) adj = path[1];
+        }
+        move = adj;
     } else {
-        if (!camper) {
-            //if no camper and hunter is shortest dist to castle dracula, move towards castle dracula
-            int hunterDist[NUM_HUNTERS] = {UNKNOWN_LOCATION,UNKNOWN_LOCATION,UNKNOWN_LOCATION,UNKNOWN_LOCATION};
-            int closestHunter = PLAYER_LORD_GODALMING;
-            LocationID adj;
-            for (i = PLAYER_LORD_GODALMING; i < NUM_HUNTERS; i++) {
-                hunterDist[i] = findShortestPath(getLocation(gameState, i), CASTLE_DRACULA, path, ANY, round);
-                if ((hunterDist[closestHunter] > hunterDist[i]) || (hunterDist[closestHunter] == UNKNOWN_LOCATION)) adj = path[1];
-            }
-			
-			move = adj;
-        } else {
-            LocationID target = UNKNOWN_LOCATION;
-            int j;
-            //set target to message history
-            if (getLatestMessageLoc(gameState) != UNKNOWN_LOCATION) target = getLatestMessageLoc(gameState);
-            
-            for (i = TRAIL_SIZE - 1; i >= 0 ; i--) { //locations newer in trail will override older ones
-                LocationID draculaLoc[TRAIL_SIZE];
-                getHistory (gameState, PLAYER_DRACULA, draculaLoc);
-                //if we have any useful info on his location...
-                if (draculaLoc[i] != CITY_UNKNOWN && draculaLoc[i] != SEA_UNKNOWN && draculaLoc[i] != HIDE) {
-                    if (draculaLoc[i] >= DOUBLE_BACK_1 && draculaLoc[i] <= DOUBLE_BACK_5) { //double back found
-                        int dbVal = i - (draculaLoc[i] - DOUBLE_BACK_1 + 1);
-                        if (dbVal >= 0) { //double back location still in trail; segfault prevention
-                            LocationID dbLoc = draculaLoc[dbVal];
-                            if (dbLoc != CITY_UNKNOWN && dbLoc != SEA_UNKNOWN && dbLoc != HIDE) target = dbLoc;
-                        }
-                    } else { //we found his location!
-                        msg = locations[draculaLoc[i]];
-                        target = draculaLoc[i];
+        LocationID target = UNKNOWN_LOCATION;
+        int j;
+        //set target to message history
+        if (getLatestMessageLoc(gameState) != UNKNOWN_LOCATION) target = getLatestMessageLoc(gameState);
+        
+        for (i = TRAIL_SIZE - 1; i >= 0 ; i--) { //locations newer in trail will override older ones
+            LocationID draculaLoc[TRAIL_SIZE];
+            getHistory (gameState, PLAYER_DRACULA, draculaLoc);
+            //if we have any useful info on his location...
+            if (draculaLoc[i] != CITY_UNKNOWN && draculaLoc[i] != SEA_UNKNOWN && draculaLoc[i] != HIDE) {
+                if (draculaLoc[i] >= DOUBLE_BACK_1 && draculaLoc[i] <= DOUBLE_BACK_5) { //double back found
+                    int dbVal = i - (draculaLoc[i] - DOUBLE_BACK_1 + 1);
+                    if (dbVal >= 0) { //double back location still in trail; segfault prevention
+                        LocationID dbLoc = draculaLoc[dbVal];
+                        if (dbLoc != CITY_UNKNOWN && dbLoc != SEA_UNKNOWN && dbLoc != HIDE) target = dbLoc;
                     }
+                } else { //we found his location!
+                    msg = locations[draculaLoc[i]];
+                    target = draculaLoc[i];
                 }
             }
-            if (target != UNKNOWN_LOCATION) target = getLocation(gameState, id); //location unknown - move randomly
-            
-            int amtLocs;
-            LocationID *adj = connectedLocations(&amtLocs, getLocation(gameState,id), id, round, ANY, g);
-            //Note: Dracula cannot visit any location currently in his trail - hunters should not visit target itself!
-            while (adj[rand() % amtLocs] == target) move = adj[rand() % amtLocs];
         }
+        if (target != UNKNOWN_LOCATION) target = getLocation(gameState, id); //location unknown - move randomly
+        
+        int amtLocs;
+        LocationID *adj = connectedLocations(&amtLocs, getLocation(gameState,id), id, round, ANY, g);
+        //Note: Dracula cannot visit any location currently in his trail - hunters should not visit target itself!
+        while (adj[rand() % amtLocs] == target) move = adj[rand() % amtLocs];
     }
+    if (move == CASTLE_DRACULA && camper) { //don't double up campers!
+        LocationID *adj = connectedLocations(&amtLocs, getLocation(gameState,id), id, round, ANY, g);
+        while (adj[rand() % amtLocs] == target || adj[rand() % amtLocs] == CASTLE_DRACULA) move = adj[rand() % amtLocs];
+    }
+        
 	registerBestPlay(locations[move], msg);
     destroyGraph(g);
 }
